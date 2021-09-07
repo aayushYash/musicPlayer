b5c98b103f8dfb80746f8c3ea4d6b7aa0e68bbd2
aayushYash <78374945+aayushYash@users.noreply.github.com>
2021-09-08 01:25:30 +0530

    Update music.js

--- music.js
+++ music.js
@@ -30,8 +30,48 @@ const body=document.querySelector('body');
         const progress=document.querySelector('#seekbar');
         const below=document.querySelector('.below');
 
+        const slider=document.querySelector('#duration_slider');
+        const show_duration=document.querySelector('#show_duration')
 
-        const animationSpan=below.children;      
+        const recent_volume = document.querySelector('#volume');
+        const volume_show = document.querySelector('.volume_show');
+
+        const auto_play=document.querySelector('#auto');
+
+        const present=document.querySelector('#present');
+        const total=document.querySelector('#total');
+
+        let timer;
+
+        let autoplay = 0;
+        
+
+        const animationSpan=below.children;
+        // volume_show.innerHTML=recent_volume.value;
+       
+        
+
+        // function playAnimation(){
+        //     for(var i=0;i<belowSpan.length;i++)
+        //     {
+        //         belowSpan[i].style.webkitAnimation="playingmusic 2s linear infinite";
+        //         belowSpan[i].style.webkitAnimationDelay= "calc(var(--i) * 0.1s)";
+        //     }
+        // }
+
+        // function pauseAnimation(){
+        //     for(var i=0;i<belowSpan.length;i++)
+        //     {
+        //         belowSpan[i].style.webkitAnimation=" ";
+        //     }
+        // }
+
+
+
+        
+
+
+        
 
         
 
@@ -106,21 +146,27 @@ const body=document.querySelector('body');
             },
         ];
 
+
+        
+
         let song_Playing=false;
+
+        // audio.addEventListener("timeupdate",function(){
+        //         progress.value=( this.currentTime/this.duration )*10;
+        //     });
 
         // play song
         function playSong(){
             song_Playing=true;
             audio.play();
             playPause.classList.add('active');
-                
+
             // onclick animation start
            for(var i=0;i<animationSpan.length;i++)
            {
-              animationSpan[i].classList.add('animSpan');
+                animationSpan[i].classList.add('animSpan');
            }
 
-            
             //change icon
             playPause.innerHTML='<ion-icon name="pause-outline"></ion-icon>'
             
@@ -131,11 +177,15 @@ const body=document.querySelector('body');
             audio.pause();
             playPause.classList.remove('active');
 
-            // onclick animation stop
+             // onclick animation stop
             for(var i=0;i<animationSpan.length;i++)
             {
                  animationSpan[i].classList.remove('animSpan');
             }
+
+            // audio.addEventListener("timeupdate",function(){
+            //     progress.value=( 5/10 );
+            // });
            
             // change icon
             playPause.innerHTML='<ion-icon name="play-outline"></ion-icon>'
@@ -145,30 +195,49 @@ const body=document.querySelector('body');
         playPause.addEventListener("click", () => (song_Playing?
         pauseSong():playSong()))
 
+        // playPause.addEventListener("click", () => (song_Playing?
+        //     playAnimation():pauseAnimation()))
+
         //load song
-        function loadSong(songList){
-            title.textContent= songList.songName;
-            audio.src = songList.path;
-            image.style.backgroundImage = songList.image;
-            download.href=songList.path;
-            download.download=songList.songName;
-            audio.addEventListener('ended',function(){
-                this.currentTime=0;
-                this.play();
-            },false);
-        }
+        function loadSong(i){
+            clearInterval(timer);
+            reset_slider();
+            title.textContent= songList[i].songName;
+            audio.src = songList[i].path;
+            image.style.backgroundImage = songList[i].image;
+            download.href=songList[i].path;
+            download.download=songList[i].songName;
+
+            total.innerHTML = songList.length;
+            present.innerHTML= i+1;
+            timer = setInterval(range_slider,1000);
+
+
+            // audio.addEventListener('ended',function(){
+            //     this.currentTime=0;
+            //     this.play();
+            // },false);
 
            
 
-
-        //  current song
-        let i=1;
+//             $('#player').on('timeupdate', function() {
+//     $('#seekbar').attr("value", this.currentTime / this.duration);
+// });
+            
+            // audio.onprogress= function(){
+            // progress.value=this.currentTime/this.duration;
+            // };
+        }
+        //  ccurrent song
+        let i=0;
 
         // onload select first song from list
-        loadSong(songList[i])
-                
-                
-                
+        loadSong(i)
+
+
+        function reset_slider(){
+            slider.value=0;
+        }
 
         function prevSong(){
             i--;
@@ -176,7 +245,7 @@ const body=document.querySelector('body');
             {
                 i=songList.length-1;
             }
-            loadSong(songList[i]);
+            loadSong(i);
             playSong();
         }
         prev.addEventListener("click",prevSong)
@@ -187,9 +256,61 @@ const body=document.querySelector('body');
             {
                 i=0;
             }
-            loadSong(songList[i]);
+            loadSong(i);
             playSong();
         }
         next.addEventListener("click",nextSong)
+
+        function mute_sound(){
+            audio.volume=0;
+            volume.value =0;
+            volume_show.innerHTML=0;
+        }
+
+        function volume_change(){
+            // alert("hello");
+            volume_show.innerHTML= recent_volume.value;
+            audio.volume=recent_volume.value / 100;  
+        }
+
+        function change_duration(){
+            slider_position = audio.duration * (slider.value/100);
+            audio.currentTime = slider_position;
+        }
+
+        function autoplay_switch(){
+            auto_play.classList.toggle('active');
+            if(autoplay==1)
+            {
+                autoplay=0;
                 
-        
+            }
+            else{
+                autoplay=1;
+            }
+        }
+
+        function range_slider(){
+            let position = 0;
+
+            if(!isNaN(audio.duration))
+            {
+                position=audio.currentTime * (100/audio.duration);
+                slider.value = position;
+            }
+
+
+
+         if(audio.ended)
+            {
+
+                // auto_play.style.background="red";
+                if(autoplay==1)
+                {
+                    i+=1;
+                    loadSong(i);
+                    playSong();
+                }
+            }
+
+        }
